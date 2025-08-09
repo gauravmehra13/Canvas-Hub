0f7c4405b2d813154c82532a4079d31952cae642
gauravmehra13 <gauravmehra2470@gmail.com>
2025-08-09 13:30:59 +0530

    cors fix 2

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -70,7 +70,7 @@ exports.login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000, // 1 day
     });
     res.status(200).json({ user: { id: user._id, username } });
